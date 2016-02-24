29cddf537e4593120bc086aa2b3c74fab9f98ed3
Kevin Carmody <kevin@skinofstars.com>
2016-02-24 09:33:35 +0000

    show off the dumb component style

--- app/components/Card/index.jsx
+++ app/components/Card/index.jsx
@@ -2,22 +2,18 @@ import React from 'react';
 
 import './style';
 
-const Card = React.createClass({
-  render() {
-    return (
-      <article className="swimlane__card pure-g">
-        <div className="swimlane__card-border">
-          <div className="pure-u-1-5 text-center">
-            <img className="swimlane__user-image" src="https://c2.staticflickr.com/8/7146/6685872171_98eb3d6cb5_z.jpg" />
-          </div>
-          <div className="pure-u-4-5">
-            <p className="swimlane__task">Buy new computer</p>
-            <time className="swimlane__date">No due date</time>
-          </div>
-        </div>
-      </article>
-    );
-  }
-});
+const Card = (props) => (
+  <article className="swimlane__card pure-g">
+    <div className="swimlane__card-border">
+      <div className="pure-u-1-5 text-center">
+        <img className="swimlane__user-image" src="https://c2.staticflickr.com/8/7146/6685872171_98eb3d6cb5_z.jpg" />
+      </div>
+      <div className="pure-u-4-5">
+        <p className="swimlane__task">Buy another computer</p>
+        <time className="swimlane__date">No due date</time>
+      </div>
+    </div>
+  </article>
+)
 
 export default Card;
--- app/components/Header/index.jsx
+++ app/components/Header/index.jsx
@@ -2,20 +2,16 @@ import React from 'react';
 
 import './style';
 
-const Header = React.createClass({
-  render() {
-    return(
-      <header className="header">
-        <div className="header__col">
-          <img className="header__logo" src="logo.png" />
-          <div className="header__breadcrumbs">Workspaces > Smoof > Project name</div>
-        </div>
-        <div className="header__col header__col--right">
-          <button className="header__cta cta">Open asana</button>
-        </div>
-    </header>
-    );
-  }
-});
+const Header = (props) => (
+  <header className="header">
+    <div className="header__col">
+      <img className="header__logo" src="logo.png" />
+      <div className="header__breadcrumbs">Workspaces > Smoof > Project name</div>
+    </div>
+    <div className="header__col header__col--right">
+      <button className="header__cta cta">Open asana</button>
+    </div>
+  </header>
+)
 
 export default Header;
