e1322fd9ddda700cb0e63fe7493311c4753af482
Alex Dunne <alex@tkstarley.co.uk>
2016-03-03 10:42:19 +0000

    Returned to using single webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,48 @@ var rucksack = require('rucksack-css');
 var webpack  = require('webpack');
 
 var NODE_ENV = JSON.stringify(process.env.NODE_ENV || 'development');
+var isDevelopment = (NODE_ENV == JSON.stringify('development'));
+var isProduction = (NODE_ENV == JSON.stringify('production'));
+
+console.log('NODE_ENV', NODE_ENV);
+
+var devTool = isDevelopment ? 'eval' : 'source-map';
+
+var entry = [path.resolve(__dirname, 'app/index.jsx')]
+if (isDevelopment) {
+  entry.push('webpack/hot/dev-server', 'webpack-dev-server/client?http://localhost:8080');
+}
+
+var plugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: NODE_ENV,
+      CLIENT_ID: JSON.stringify(process.env.CLIENT_ID)
+    }
+  })
+];
+
+if (isDevelopment) {
+  plugins.push(new webpack.HotModuleReplacementPlugin());
+}
+
+if (isProduction) {
+  plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      compress: {
+        warnings: false
+      }
+    })
+  );
+}
 
 var config = {
-  entry: [
-    'webpack-dev-server/client?http://localhost:8080',
-    'webpack/hot/dev-server',
-    path.resolve(__dirname, 'app/index.jsx')
-  ],
+  devtool: devTool,
+  entry: entry,
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js',
+    filename: 'bundle.js'
   },
   module: {
     preLoaders: [
@@ -29,22 +61,14 @@ var config = {
       autoprefixer: true
     })
   ],
-  node: {
-    readline: 'empty'
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: NODE_ENV,
-        CLIENT_ID: JSON.stringify(process.env.CLIENT_ID)
-      }
-    }),
-    new webpack.HotModuleReplacementPlugin()
-  ],
   resolve: {
     extensions: ['', '.js', '.jsx', '.json', '.scss'],
     modulesDirectories: ['node_modules']
-  }
+  },
+  node: {
+    readline: 'empty'
+  },
+  plugins: plugins,
 };
 
 module.exports = config;
