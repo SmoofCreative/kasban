060ee5bad80f7d74555b2f5524d922fdd90360dc
alexdunne <hi@alexdunne.net>
2016-04-18 11:48:02 +0100

    Added google analytics module to associate with users

---
+++ app/utils/GoogleAnalytics.js
@@ -0,0 +1,9 @@
+import ga from 'react-ga';
+
+const options = {
+  debug: process.env.NODE_ENV === 'development'
+};
+
+ga.initialize(process.env.GOOGLE_ANALYTICS_ID, options);
+
+export default ga;
